737b51e61cf13670221f4f7fa0e1f1bd3212ba91
Daniel <skalskidaniel@icloud.com>
2024-12-27 23:57:53 +0100

    Add database class

--- include/Database.h
+++ include/Database.h
@@ -1,16 +1,27 @@
 //
-// Created by Daniel Skalski on 26/12/2024.
+// Created by Wiktor on 27/12/2024.
 //
 
 #ifndef DATABASE_H
 #define DATABASE_H
-
-
+#include <vector>
+#include <Board.h>
 
 class Database {
+public:
+    // storing {DIFFICULTY, {initialState, solution} }
+    std::vector<int, std::pair<Board, Board>> savedBoards;
+    // storing current state of a game in case we want stop for a while and resume later
+    Board currentState;
+    // best score so far
+    int bestScore;
 
+    Database();
+
+    void loadData();
+
+    void addBoard(std::string board, std::string solution);
 };
-
 
 
 #endif //DATABASE_H
--- src/Database.cpp
+++ src/Database.cpp
@@ -1,5 +1,42 @@
 //
-// Created by Daniel Skalski on 26/12/2024.
+// Created by Wiktor on 27/12/2024.
 //
 
 #include "Database.h"
+#include <fstream>
+
+Database::Database() {
+    bestScore = 0;
+    loadData();
+}
+
+void Database::loadData() {
+    // TODO
+    // zapisane tablice musza byc wpisywane jako klasa Board a nie string
+    std::ifstream file("saves/boards.csv");
+
+    if (!file.is_open()) {
+        throw std::runtime_error("Failed to load saved boards!");
+    }
+
+    std::string line;
+    while (std::getline(file, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        std::string board = "";
+        std::string solution = "";
+        int i = 0;
+        while(line[i] != ';') {
+            if (line[i++] == ';') {
+                break;
+            }
+            board += line[i++];
+        }
+        while(i < line.size()) {
+            solution += line[i++];
+        }
+        savedBoards.push_back({board, solution});
+    }
+}
+
